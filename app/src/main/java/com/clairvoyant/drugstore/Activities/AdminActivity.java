4d3dc8b8c1fbbe6d331fb497733bc56b17b982bb
tanzeer <tanzeerhossain@zoho.com>
2020-05-03 02:23:56 +0600

    admin panel ui added

---
+++ app/src/main/java/com/clairvoyant/drugstore/Activities/AdminActivity.java
@@ -0,0 +1,16 @@
+package com.clairvoyant.drugstore.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.clairvoyant.drugstore.R;
+
+public class AdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+}
