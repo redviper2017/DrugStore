daade5ac384b2154dce56f7da946b601b22de19c
redviper2017 <tanzeerhossain@zoho.com>
2020-05-28 00:30:32 +0600

    payment functionality done 90%

--- app/src/main/java/com/clairvoyant/drugstore/Activities/CheckoutActivity.java
+++ app/src/main/java/com/clairvoyant/drugstore/Activities/CheckoutActivity.java
@@ -39,12 +39,14 @@ import java.util.Map;
 public class CheckoutActivity extends AppCompatActivity {
 
     private ListView listView;
-    private CardView confirmOrderButton;
-    public EditText deliveryAddressText;
+    private CardView confirmOrderButton, bkashPaymentButton, cashPaymentButton;
+    public EditText deliveryAddressText, bkashTrxText;
     public TextView totalPriceText;
     public List<CartProduct> cartProductsFinal;
     private static final String TAG = "CheckoutActivity";
     private double totalPrice;
+    private String paymentMethod;
+    private String bkashTrxID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,14 +54,30 @@ public class CheckoutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_checkout);
         listView = findViewById(R.id.order_summary_list);
         confirmOrderButton = findViewById(R.id.place_order_card);
+        bkashPaymentButton = findViewById(R.id.bkash_payment_card);
+        cashPaymentButton = findViewById(R.id.cash_payment_card);
         deliveryAddressText = findViewById(R.id.delivery_address_text);
         totalPriceText = findViewById(R.id.total_price_text);
+        bkashTrxText = findViewById(R.id.bkash_trx_text);
+
         getCartProducts();
         confirmOrderButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (cartProductsFinal != null)
                     storeOrderToFirestore();
+            }
+        });
+        bkashPaymentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                paymentMethod = "bkash";
+            }
+        });
+        cashPaymentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                paymentMethod = "cash";
             }
         });
     }
@@ -91,7 +109,7 @@ public class CheckoutActivity extends AppCompatActivity {
                 CustomListAdapter customListAdapter = new CustomListAdapter(cartProducts);
                 listView.setAdapter(customListAdapter);
 
-                for (CartProduct cartProduct: cartProducts)
+                for (CartProduct cartProduct : cartProducts)
                     totalPrice += cartProduct.getPrice() * cartProduct.getSelectedQty();
                 totalPriceText.setText(String.valueOf(totalPrice));
             }
@@ -102,17 +120,34 @@ public class CheckoutActivity extends AppCompatActivity {
     }
 
     public void storeOrderToFirestore() {
+        Map<String, Object> nestedFinalOrder = new HashMap<>();
+
+        if (TextUtils.isEmpty(paymentMethod))
+            Toast.makeText(getApplicationContext(), "Select a payment method first!", Toast.LENGTH_SHORT).show();
+        else {
+            if (paymentMethod.equals("bkash")) {
+                if (TextUtils.isEmpty(bkashTrxText.getText())) {
+                    bkashTrxText.setError("Enter TrxID");
+                    bkashTrxText.requestFocus();
+                } else {
+                    bkashTrxID = bkashTrxText.getText().toString().trim();
+                    nestedFinalOrder.put("paymentWith", paymentMethod);
+                    nestedFinalOrder.put("bKashTrxID", bkashTrxID);
+                }
+            } else {
+                nestedFinalOrder.put("paymentWith", paymentMethod);
+            }
+        }
         SimpleDateFormat dateformat = new SimpleDateFormat("dd/MMM/yyyy hh:mm aa");
         String orderDate = dateformat.format(Calendar.getInstance().getTime());
 
-        Log.d(TAG,"order date = "+orderDate);
+        Log.d(TAG, "order date = " + orderDate);
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         //Create new order in Firestore
         Log.d(TAG, "final ordered products list size = " + cartProductsFinal.size());
 
         int i = 1;
 
-        Map<String, Object> nestedFinalOrder = new HashMap<>();
 
         for (CartProduct cartProduct : cartProductsFinal) {
             double orderPriceProduct = cartProduct.getPrice() * cartProduct.getSelectedQty();
@@ -125,11 +160,12 @@ public class CheckoutActivity extends AppCompatActivity {
             finalOrder.put("total", orderPriceProduct);
 
 
-            nestedFinalOrder.put(String.valueOf("product:"+i),finalOrder);
+            nestedFinalOrder.put(String.valueOf("product:" + i), finalOrder);
             i++;
         }
-        nestedFinalOrder.put("time",orderDate);
-        nestedFinalOrder.put("subTotal",totalPriceText.getText().toString());
+        nestedFinalOrder.put("time", orderDate);
+        nestedFinalOrder.put("subTotal", totalPriceText.getText().toString());
+
         if (!TextUtils.isEmpty(deliveryAddressText.getText().toString())) {
             nestedFinalOrder.put("address", deliveryAddressText.getText().toString());
             db.collection("orders").document()
@@ -145,11 +181,12 @@ public class CheckoutActivity extends AppCompatActivity {
                     Log.w(TAG, "Error writing document", e);
                 }
             });
-        }else
-            Toast.makeText(getApplicationContext(),"You must provide delivery address in order to place this order!",Toast.LENGTH_SHORT).show();
-//        finalOrder.put("orderPrice",productOrderPrice);
-//        finalOrder.put("deliveryAddress",deliveryAddressText.getText().toString());
+        } else {
+            deliveryAddressText.setError("Enter a delivery address");
+            deliveryAddressText.requestFocus();
+        }
     }
+
 
     class CustomListAdapter extends BaseAdapter {
 
@@ -191,5 +228,7 @@ public class CheckoutActivity extends AppCompatActivity {
         }
     }
 }
+
+
 
 
